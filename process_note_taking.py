616f4a87e319e6fb069fddcba58ff7fa57cc0060
AleksandrSim <46563315+AleksandrSim@users.noreply.github.com>
2022-05-03 23:47:40 -0500

    Generated notes, using T5 large mode. The model takes 2000 tokens at the time and summarizes them. The number of taken can be changed. After the evaluation, the next steps will be to investigate how the quality of notes can be increased for specific passages that user needs

--- process_note_taking.py
+++ process_note_taking.py
@@ -1,13 +1,39 @@
 import argparse
 
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import T5ForConditionalGeneration,TransfoXLConfig, TransfoXLModel, T5Tokenizer,XLNetConfig,XLNetModel, MT5Model, XLNetTokenizer, XLMModel
 import json
 import os
+import time
 
 def prepare_predictor(model_name ='TF5_cond'):
     if model_name =='TF5_cond':
         model = T5ForConditionalGeneration.from_pretrained("t5-large")
         tokenizer = T5Tokenizer.from_pretrained("t5-large")
+
+    elif model_name == 'MT5Model':
+        model = T5ForConditionalGeneration.from_pretrained("google/mt5-base")
+        tokenizer = T5Tokenizer.from_pretrained("t5-base")
+
+    elif model_name =='XLNet':
+        config = XLNetConfig()
+        model =XLNetModel(config)
+        tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
+
+
+    elif model_name == 'XLNETRANSFORMER':
+        config =  TransfoXLConfig()
+        model = TransfoXLModel(config)
+        tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
+
+    elif model_name == 'MT5ModelXL':
+        model = T5ForConditionalGeneration.from_pretrained("google/mt5-xl")
+        tokenizer = T5Tokenizer.from_pretrained("t5-base")
+
+
+
+
+
+
 
     return model, tokenizer
 
@@ -20,7 +46,7 @@ def load_dataset(dataset_name):
             return data
 
 
-def generate_notes(article,model, tokenizer,token_n_per_iter = 2000, max_length_val=360, min_length_val= 120, length_penalty_val= 2.0, return_tensors="pt", file_name ='recurrent_neural_network'):
+def generate_notes(article,model, tokenizer,token_n_per_iter = 2000, max_length_val=260, min_length_val= 120, length_penalty_val= 2.0, return_tensors="pt", file_name ='recurrent_neural_network'):
     article_length = len(article)
     current_index = 0
     note=1
@@ -33,8 +59,12 @@ def generate_notes(article,model, tokenizer,token_n_per_iter = 2000, max_length_
             break
         model_name = model_name + i
 
+    model_name = 'TFT5+BASE'
+
+
     while current_index <article_length:
-        inputs = tokenizer.encode("summarize: " + article[current_index:current_index+token_n_per_iter], return_tensors="pt", max_length=512,truncation=True)
+        current_time =time.time()
+        inputs = tokenizer.encode("summarize: " + article[current_index:current_index+token_n_per_iter], return_tensors="pt", max_length=300,truncation=True)
         outputs = model.generate(inputs,max_length=max_length_val,min_length=min_length_val,
             length_penalty=length_penalty_val,num_beams=7,early_stopping=True)
         outputs = tokenizer.decode(outputs[0])
@@ -44,7 +74,10 @@ def generate_notes(article,model, tokenizer,token_n_per_iter = 2000, max_length_
 
         path = 'notes_from_article/' + file_name + '/' + model_name+'/'+str(note)+'_'+ str(current_index)
         with open('notes_from_article/' + file_name + '/' + model_name +'/' + str(note)+'_'+ str(current_index) +'json', 'w') as fp:
+            current_time = time.time() - current_time
+            print('time_for_this_article--------->'+str(current_time ))
             json.dump(outputs, fp)
+
         print('note_saved----------->' +str(note))
         note +=1
 
