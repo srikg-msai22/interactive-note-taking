c53f469e5c401469a3929b01ef983b766cbf39f8
AleksandrSim <46563315+AleksandrSim@users.noreply.github.com>
2022-05-31 23:04:23 -0500

    fine tunning and additional articles

---
+++ summarization_paragraphs.py
@@ -0,0 +1,46 @@
+
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+import json
+import os
+import time
+
+
+
+def generate_folders(folder):
+    to_iterate = os.listdir('original_articles/'+str(folder))[:-2]
+    return to_iterate
+
+
+def summarization(folder):
+    model = T5ForConditionalGeneration.from_pretrained("t5-large")
+    tokenizer = T5Tokenizer.from_pretrained("t5-large")
+    current_time = time.time()
+
+    for file_name in folder:
+        print('test----->'+file_name)
+        fine_label = file_name[:2]
+
+        with open('original_articles/'+ file_name[0:2] + '_edited' + '/'+str(file_name), 'r') as f:
+            data = json.load(f)
+        current_time = time.time()
+        inputs = tokenizer.encode("summarize: " +data, return_tensors="pt", max_length=512, truncation=True)
+        outputs = model.generate(inputs,max_length=400,min_length= 200,length_penalty=2.0,num_beams=7,early_stopping=True)
+        outputs = tokenizer.decode(outputs[0])
+
+        if not os.path.isdir('notes_from_article/' + fine_label):
+            os.mkdir('notes_from_article/' + fine_label)
+        with open('notes_from_article/' + fine_label + '/'+ str(file_name[3:]), 'w') as fp:
+            current_time = time.time() - current_time
+            print('time_for_this_article--------->' + str(current_time))
+            json.dump(outputs, fp)
+
+
+
+
+if __name__ =='__main__':
+    with open('original_articles/P1/P1_S01.json', 'r') as f:
+        data = json.load(f)
+    folder = generate_folders('P1_edited')
+    summarization(folder)
+
+
